aa421e4e4109f7d67c41e4a4fef4b02623326084
DavidNjoku72 <C23440392@mytudublin.ie>
2026-05-17 14:47:51 +0200

    Initial backend setup with Spring Boot and Swagger

---
+++ src/main/java/com/david/pollhub/MyprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.david.pollhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectBackendApplication.class, args);
+	}
+
+}
